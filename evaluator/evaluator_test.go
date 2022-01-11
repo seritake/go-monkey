50a7e70c763d9db1517018cde28bf1bd2079abb5
Takeru Serikawa <takeru.serikawa@gmail.com>
2022-01-11 20:16:02 +0900

    add object

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testIntegerObject(t *testing.T, evaluated object.Object, expected int64) bool {
+	result, ok := evaluated.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not integer")
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value")
+		return false
+	}
+	return true
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
